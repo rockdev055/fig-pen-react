d0ab610b6c9196a3d0d1d25a5dc19dd83d1579a0
rockdev55 <steve264225@gmail.com>
2019-11-14 15:54:07 -0800

    Fix error

--- src/containers/PinsList.js
+++ src/containers/PinsList.js
@@ -77,10 +77,8 @@ const PinsList = ({ pins, searchedPins, searchPins }) => {
                       height={130}
                       width={100}
                       alt="Remy Sharp"
-                      src={
-                        `${https://cryptic-beyond-25854.herokuapp.com}/
-                        ${pin.photo_url}`
-                      }
+                      src={`https://cryptic-beyond-25854.herokuapp.com/
+                        ${pin.photo_url}`}
                     />
                   </Link>
                 </ListItemAvatar>
