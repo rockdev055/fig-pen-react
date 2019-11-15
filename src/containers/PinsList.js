7807bacd94e00c9a8036b74f1fd46136a4a3db2f
rockdev55 <steve264225@gmail.com>
2019-11-14 16:04:00 -0800

    Update pin image in index

--- src/containers/PinsList.js
+++ src/containers/PinsList.js
@@ -77,8 +77,7 @@ const PinsList = ({ pins, searchedPins, searchPins }) => {
                       height={130}
                       width={100}
                       alt="Remy Sharp"
-                      src={`https://cryptic-beyond-25854.herokuapp.com
-                        ${pin.photo_url}`}
+                      src={`https://cryptic-beyond-25854.herokuapp.com${pin.photo_url}`}
                     />
                   </Link>
                 </ListItemAvatar>
