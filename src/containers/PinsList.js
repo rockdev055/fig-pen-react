56e1c8305c400afaf5f88c25367cf10ef4c4406d
rockdev55 <steve264225@gmail.com>
2019-12-14 19:30:11 -0800

    Update pins list component

--- src/containers/PinsList.js
+++ src/containers/PinsList.js
@@ -1,7 +1,4 @@
 import React, { Component, useState, useEffect } from "react";
-import { connect } from "react-redux";
-import { searchPins, clearSearch } from "../redux/actions/pins";
-import { useSpring, animated } from "react-spring";
 import { makeStyles } from "@material-ui/core/styles";
 
 import { Link } from "react-router-dom";
@@ -34,136 +31,69 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const PinsList = ({ pins, searchedPins, searchPins, clearSearch }) => {
-  useEffect(() => {
-    clearSearch();
-  }, [clearSearch]);
+const PinsList = ({ pinsToDisplay }) => {
   const classes = useStyles();
-  const props = useSpring({
-    config: { duration: 500 },
-    to: { opacity: 1 },
-    from: { opacity: 0, height: 600 }
-  });
-  const [search, setSearch] = useState("");
-  if (pins.length === 0) {
-    return <p>Loading...</p>;
-  }
 
-  const pinsToDisplay =
-    searchedPins.length === 0 && search === "" ? pins : searchedPins;
   return (
-    <animated.div style={props}>
-      <List className={classes.root}>
-        <Typography style={{ marginTop: "20px" }} variant="h2" gutterBottom>
-          Catalog
-        </Typography>
-        <Container>
-          <TextField
-            id="filled-full-width"
-            label="Search"
-            style={{ margin: 8 }}
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true
-            }}
-            onChange={e => {
-              setSearch(e.target.value);
-              searchPins(e.target.value);
-            }}
-          />
-          <FormControl component="fieldset">
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    value={"exclusive"}
-                    color="default"
-                    onChange={() => console.log("hello")}
+    <List className={classes.root}>
+      <Container>
+        {pinsToDisplay.map(pin => (
+          <>
+            <ListItem alignItems="center">
+              <ListItemAvatar style={{ marginRight: "3rem" }}>
+                <Link to={`/pins/${pin.id}`}>
+                  <img
+                    height={130}
+                    width={100}
+                    alt={pin.name}
+                    src={
+                      pin.photo_url
+                        ? process.env.NODE_ENV === "development"
+                          ? `http://localhost:3001${pin.photo_url}`
+                          : `https://cryptic-beyond-25854.herokuapp.com${pin.photo_url}`
+                        : "https://via.placeholder.com/520x720"
+                    }
                   />
-                }
-                label={"Exclusive"}
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    value={"exclusive"}
-                    color="default"
-                    onChange={() => console.log("hello")}
-                  />
-                }
-                label={"Exclusive"}
-              />
-            </FormGroup>
-          </FormControl>
-          {search !== "" && searchedPins.length === 0 && (
-            <h1>No Search Results</h1>
-          )}
-          {pinsToDisplay.map(pin => (
-            <>
-              <ListItem alignItems="center">
-                <ListItemAvatar style={{ marginRight: "3rem" }}>
-                  <Link to={`/pins/${pin.id}`}>
-                    <img
-                      height={130}
-                      width={100}
-                      alt={pin.name}
-                      src={
-                        pin.photo_url
-                          ? process.env.NODE_ENV === "development"
-                            ? `http://localhost:3001${pin.photo_url}`
-                            : `https://cryptic-beyond-25854.herokuapp.com${pin.photo_url}`
-                          : "https://via.placeholder.com/520x720"
-                      }
-                    />
-                  </Link>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={
-                    <Link
-                      to={`/pins/${pin.id}`}
-                      style={{ textDecoration: "none" }}
+                </Link>
+              </ListItemAvatar>
+              <ListItemText
+                primary={
+                  <Link
+                    to={`/pins/${pin.id}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <Typography
+                      component="span"
+                      className={classes.inline}
+                      color="textPrimary"
                     >
-                      <Typography
-                        component="span"
-                        className={classes.inline}
-                        color="textPrimary"
-                      >
-                        {pin.name}
-                      </Typography>
-                    </Link>
-                  }
-                  secondary={
-                    <React.Fragment>
-                      <Typography
-                        component="span"
-                        variant="body2"
-                        className={classes.inline}
-                        color="textPrimary"
-                      >
-                        FiGPiN ID:
-                      </Typography>
-                      {" " + pin.figpin_id}
-                      <br />
-                      {pin.line && `Line: ${pin.line.name}`}
-                    </React.Fragment>
-                  }
-                />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-            </>
-          ))}
-        </Container>
-      </List>
-    </animated.div>
+                      {pin.name}
+                    </Typography>
+                  </Link>
+                }
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="textPrimary"
+                    >
+                      FiGPiN ID:
+                    </Typography>
+                    {" " + pin.figpin_id}
+                    <br />
+                    {pin.line && `Line: ${pin.line.name}`}
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </>
+        ))}
+      </Container>
+    </List>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    pins: state.pins.all,
-    searchedPins: state.pins.searchedPins
-  };
-};
-
-export default connect(mapStateToProps, { searchPins, clearSearch })(PinsList);
+export default PinsList;
